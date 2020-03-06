a35892122fa53102a55e106223782700c7b6d978
opheliecoiffier <60701868+opheliecoiffier@users.noreply.github.com>
2020-03-06 20:10:45 +0100

    "utils"

--- utils.py
+++ utils.py
@@ -2,17 +2,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+    
 def affichage(fonction, matrice):
     """Cette fonction nous permet d'afficher 10 itérations d'une matrice
     réparties sur 2 lignes et 5 colonnes.
     """
     tab = list()
-    plt.subplots(2, 5)
     for i in range(10):
+        plt.subplot(2, 5, i+1)  #afficher les figures sur 2 lignes/5 colonnes.
         matrice_bloc = fonction(matrice)
-        tab.append(matrice_bloc)
-        plt.imshow(tab[i])
+        tab.append(matrice_bloc)  #on place les itérations dans notre liste
+        plt.imshow(tab[i])  #affichage des figures
+        plt.suptitle("itérations de 1 à 10 d'une matrice stable")
         plt.title("Itération" + str(i+1))
 
     
